ade8a5d9c8596c364936ab5c3d512ae4b8f41e46
joao <joaootaviors@gmail.com>
2025-07-22 16:45:28 +1000

    config: add devnet and mainnet providers to Anchor.toml

--- programs/hello-world/src/lib.rs
+++ programs/hello-world/src/lib.rs
@@ -1,16 +1,20 @@
 use anchor_lang::prelude::*;
 
+// This macro sets the public key for the program.
 declare_id!("9j4hBaQSzLMxdRKdPSveY7fwrPQwdUkvPf99tNyWBcGZ");
 
+// the #[program] macro marks this module as a Solana program
 #[program]
 pub mod hello_world {
-    use super::*;
+    use super::*; // Imports all items from the parent module scope (Context, Result, msg!, etc.)
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    // A function defines an instruction that can be called from clients
+    // The Context parameter contains Accounts needed for this instruction and program metadata
+    pub fn hello(_ctx: Context<Hello>) -> Result<()> {
+        msg!("Hello"); // msg!() is Anchor's logging macro - this prints "Hello" to the program logs
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Hello {}
