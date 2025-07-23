304bbde9f50a9264f207b5dda67aa190389757a1
joao <joaootaviors@gmail.com>
2025-07-23 14:16:26 +1000

    add directory client/ for rust binaries

--- programs/hello-world/src/lib.rs
+++ programs/hello-world/src/lib.rs
@@ -9,7 +9,7 @@ pub mod hello_world {
     use super::*; // Imports all items from the parent module scope (Context, Result, msg!, etc.)
 
     // A function defines an instruction that can be called from clients
-    // The Context parameter contains Accounts needed for this instruction and program metadata
+    // The Context parameter contains Accounts naeeded for this instruction and program metadata
     pub fn hello(_ctx: Context<Hello>) -> Result<()> {
         msg!("Hello"); // msg!() is Anchor's logging macro - this prints "Hello" to the program logs
         Ok(())
