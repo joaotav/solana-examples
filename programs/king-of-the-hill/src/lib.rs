7f951554e22e71df7202cbeca7e94d9ed5ac22a1
joao <joaootaviors@gmail.com>
2025-08-04 16:25:36 +1000

    feat: add initialize() and become_king() to king-of-the-hill

--- programs/king-of-the-hill/src/lib.rs
+++ programs/king-of-the-hill/src/lib.rs
@@ -68,29 +68,41 @@ pub mod king_of_the_hill {
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, 
-        payer = initial_king,
-        space = DISCRIMINATOR + GameState::INIT_SPACE, 
-        seeds = [b"game_state"], 
-        bump,
+    #[account(init, // Create a new account on the blockchain
+        payer = initial_king, // initial_king will pay the rent for creating this account
+        space = DISCRIMINATOR + GameState::INIT_SPACE, // Allocate space for the new account
+        seeds = [b"game_state"], // Creates a PDA using this seed
+        bump, // Anchor automatically stores the canonical bump used for the PDA
     )]
+    // This account will contain GameState data
     pub game_state: Account<'info, GameState>,
     #[account(mut)]
+    // initial_king needs to be mutable to pay for account creation
     pub initial_king: Signer<'info>,
     #[account(init, 
         payer = initial_king, 
         space = DISCRIMINATOR + 8, 
-        seeds = [b"prize_pool"], 
+        seeds = [b"prize_pool"], // Create a PDA using this seed
         bump,
     )]
     /// CHECK: This is okay - it's a PDA to store SOL and doesn't need a data layout
+    // This PDA will be used by the program to store the prize money.
+    // Unchecked accounts are not deserialized or validated by Anchor, they contain raw account data 
+    // from AccountInfo. They and are more efficient in this case where we only need to store 
+    // SOL and check the balance. 
     pub prize_pool: UncheckedAccount<'info>,
+    // Reference to Solana's built-in System Program, which is required for creating new
+    // accounts and transferring SOL.
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 pub struct BecomeKing<'info> {
-    #[account(mut, has_one = king,)]
+    #[account(mut, // The account is mutable because the current king needs to be updated
+        // Constraint to check if the "king" field in GameState matches the key of the "king" 
+        // field in BecomeKing
+        has_one = king, 
+    )]
     pub game_state: Account<'info, GameState>,
     #[account(mut)]
     /// CHECK: This is okay - it's only receiving SOL and we don't need any other access
@@ -103,6 +115,13 @@ pub struct BecomeKing<'info> {
     pub system_program: Program<'info, System>
 }
 
+// The #[account] attribute sets the discriminator for the new account and also
+// sets the owner of the account to the programId in the declare_id! macro
+//
+// The #[derive(InitSpace)] automatically calculates the exact space in bytes that
+// the account structure will need when stored-on chain. Variables of dynamic size
+// such as Vec<T> need to be annotated with #[max_len(COUNT)] if T is of fixed size
+// or #[max_len(COUNT), ITEM_SIZE] if T is of dynamic size, e.g., String.
 #[account]
 #[derive(InitSpace)]
 pub struct GameState {
@@ -121,5 +140,6 @@ pub enum ErrorCode {
     InvalidPrizePoolAccount,
 }
 
+// The size of the account type discriminator
 const DISCRIMINATOR: usize = 8;
 
